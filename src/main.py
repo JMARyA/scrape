80a16b26f3496fb41d683000255f514a53e9c8b6
JMARyA <jmarya@hydrar.de>
2023-06-16 02:29:31 +0200

    add more args
    - added timestamp flag for saving when the scrape was made
    - added language argument to change site language

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 #!python3
 import argparse
 from scrape.extractors import scrape_site
+from scrape.val import Language
 
 
 class ShowSitesAction(argparse.Action):
@@ -15,16 +16,35 @@ class ShowSitesAction(argparse.Action):
         parser.exit()
 
 
+class Config:
+    save_ts: bool = False
+    language: Language
+
+
 def main():
     parser = argparse.ArgumentParser(description="Web Scraper")
     parser.add_argument(
         "--sites", action=ShowSitesAction, nargs=0, help="Show all supported sites"
     )
+    parser.add_argument("-t", action="store_true", help="Store timestamp when scraping")
+    parser.add_argument(
+        "--lang",
+        choices=list(Language._member_names_),
+        default="en_US",
+        help="Desired language to scrape in",
+    )
     parser.add_argument("url", type=str, help="URL to scrape")
 
     args = parser.parse_args()
 
-    scrape_site(args.url)
+    conf = Config()
+
+    if args.t:
+        conf.save_ts = True
+
+    conf.language = Language.__members__[args.lang]
+
+    scrape_site(args.url, conf)
 
 
 if __name__ == "__main__":
--- src/scrape/extractors/__init__.py
+++ src/scrape/extractors/__init__.py
@@ -3,8 +3,10 @@ import json
 from rich import print
 import sys
 from . import igdb, steam, aur, amazon
-from ..val import Language
+from ..val import Language, printerr, printwarn
 from enum import Enum
+import datetime
+import pytz
 
 
 def present(data: dict):
@@ -21,22 +23,33 @@ class supported_sites(Enum):
     AMAZON = "www.amazon.de"
 
 
-def scrape_site(url: str):
+def language_ignored_warn(conf):
+    if conf.language != Language.en_US:
+        printwarn("Language setting is not used by this extractor")
+
+
+def scrape_site(url: str, conf):
     _url = urlparse(url)
     host = _url.hostname
 
+    ts = datetime.datetime.now(pytz.utc)
+
     match host:
         case supported_sites.IGDB.value:
+            language_ignored_warn(conf)
             data = igdb.igdb_game(url)
-            present(data)
         case supported_sites.STEAM.value:
-            data = steam.steam_game(url, Language.en_US)
-            present(data)
+            data = steam.steam_game(url, conf.language)
         case supported_sites.AUR.value:
+            language_ignored_warn(conf)
             data = aur.aur_package(url)
-            present(data)
         case supported_sites.AMAZON.value:
-            data = amazon.amazon_product(url, Language.en_US)
-            present(data)
+            data = amazon.amazon_product(url, conf.language)
         case _:
-            print("[red]Error:", "Unknown site")
+            printerr("Unknown site")
+            exit(1)
+
+    if conf.save_ts:
+        data["scraped_at"] = ts.strftime("%Y-%m-%d %H:%M:%S %z")
+
+    present(data)
